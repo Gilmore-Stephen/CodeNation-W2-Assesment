f6bc374aa225ad7cfdf1fc5020266ef94cedcbf5
gilmore-stephen <gilmore.stephen.d@gmail.com>
2024-09-06 14:36:39 +0100

    week 2 day2 assesment 1 commit

---
+++ age.js
@@ -0,0 +1,10 @@
+let age = 17
+let country = "uk"
+if (age >17 && country === "uk")  // funny with = and ===
+{
+console.log("yes i can server you");
+}
+else     
+{
+console.log("you aren't old enough or not in the UK")
+}
