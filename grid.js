2b18070dcbf6478f91487e9b392f9591f80e26b0
gilmore-stephen <gilmore.stephen.d@gmail.com>
2024-09-06 11:57:36 +0100

    week 2 day 1 assesment commit

---
+++ grid.js
@@ -0,0 +1,31 @@
+let column1 = "    |    |"
+
+let column2 = "    |    |"
+
+let column3 = "    |    |"
+
+let row1 =    "--------------"
+let column4 = "    |    |"
+
+let column5 = "    |    |"
+
+let column6 = "    |    |"
+
+let row2 =    "--------------"
+let column7 = "    |    |"
+
+let column8 = "    |    |"
+ 
+let column9 = "    |    |"
+console.log (column1)
+console.log (column2)
+console.log (column3)
+console.log (row1)
+console.log (column4)
+console.log (column5)
+console.log (column6)
+console.log (row1)
+console.log (column7)
+console.log (column8)
+console.log (column9)
+
